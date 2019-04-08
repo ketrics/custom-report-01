fbdf356ff6fe1c96a4cdee4af7d3c2c6d39119e3
cristianketrics <cristian@khn.cl>
2019-04-07 21:16:36 -0300

    initial commit

---
+++ template.jsx
@@ -0,0 +1,7 @@
+<div>
+    <img src="https://www.sermecoop.cl/sitio/images/logo.png" alt=""/>
+    
+    <google-pie-chart data={data} title="Hello 01" x_serie="name" y_serie="value1" />
+    
+    <div style={{height: 100}}/>
+</div>
