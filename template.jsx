b8223301f3ae0533aadd092a5e2c588886d8f3cc
cristianketrics <cristian@khn.cl>
2019-04-08 09:19:06 -0300

    change extension of template

--- template.jsx
+++
@@ -1,9 +0,0 @@
-<div>
-    <img src="https://www.sermecoop.cl/sitio/images/logo.png" alt=""/>
-    
-    <google-pie-chart data={data} title="Hello 01" x_serie="name" y_serie="value1" />
-
-    <google-pie-chart data={data} title="Hello 02" x_serie="name" y_serie="value2" />
-    
-    <div style={{height: 100}}/>
-</div>
